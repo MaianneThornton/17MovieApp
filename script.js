5bb056b3bc9477f6b31e9357ae6231e0fe354baa
Maianne Thornton <maianne.thornton@gmail.com>
2022-06-29 13:28:25 -0400

    feat: added pagination functionality

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3fd2be6f0c70a2a598f084ddfb75487c&page=1'
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=3fd2be6f0c70a2a598f084ddfb75487c&query="'
+const MOVIE_PAGE = 'https://www.themoviedb.org/movie/'
 
 const form = document.getElementById('form')
 const search = document.getElementById('search')
@@ -23,15 +24,16 @@ function showMovies(movies) {
 
     movies.forEach((movie) => {
         // destructuring the movie object
-        const { title, poster_path, vote_average, overview } = movie
+        const { title, poster_path, vote_average, overview, id } = movie
 
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie')
 
         movieEl.innerHTML = `
-        <img
-          src="${IMG_PATH + poster_path}"
-          alt="${title}"/>
+        <a href='${MOVIE_PAGE + id}'>
+            <img
+                src="${IMG_PATH + poster_path}"
+                alt="${title}"/>
         <div class="movie-info">
           <h3>${title}</h3>
           <span class="${getClassByRate(vote_average)}">${vote_average}</span>
@@ -40,6 +42,7 @@ function showMovies(movies) {
           <h3>Overview</h3>
           ${overview}
         </div>
+        </a>
       </div>
     `
         main.appendChild(movieEl)
