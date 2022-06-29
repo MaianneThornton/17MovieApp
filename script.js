f08503fee7b6d09d33f0dad026875339ec3543d6
Maianne Thornton <maianne.thornton@gmail.com>
2022-06-29 14:06:16 -0400

    chore: formatted code

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ async function getMovies(url) {
 
 
 function showMovies(movies) {
-    // clears the main section for overwritting
+    // clears the main section for overwriting
     main.innerHTML = ''
 
     movies.forEach((movie) => {
